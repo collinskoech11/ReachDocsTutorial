4bce67dcc6389846c848647c1e6a77c2df182a63
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-03-12 16:14:40 +0300

    rps logic update

--- examples/rps-2-rps/index.mjs
+++ examples/rps-2-rps/index.mjs
@@ -5,6 +5,11 @@ const stdlib = loadStdlib();//loads the standard library dynamically based on th
 const startingBalance = stdlib.parseCurrency(100);//defines a quantity of network tokens as the starting balance for each test account.
 const accAlice = await stdlib.newTestAccount(startingBalance);//
 const accBob = await stdlib.newTestAccount(startingBalance);
+const fmt = (x) => stdlib.formatCurrency(x, 4);//function for displaying currency amounts with up to 4 decimal places
+const getBalance = async (who) => fmt(await stdlib.balanceOf(who));// getting the balance of a participant and displaying it with up to 4 decimal places.
+//getting balance of participants before game starts
+const beforeAlice = await getBalance(accAlice);
+const beforeBob = await getBalance(accBob);
 //create test accounts with initial endowments for Alice and Bob.
 const ctcAlice = accAlice.contract(backend);//Alice deploy the application.
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo());// Bob attach to it
@@ -24,10 +29,21 @@ const Player = (Who) => ({// constructor for player implementation
 await Promise.all([
   ctcAlice.p.Alice({
     // implement Alice's interact object here
-    ...Player('Alice')
+    ...Player('Alice'),//splices the common Player interface into Alice's interface.
+    wager: stdlib.parseCurrency(5),//defines her wager as 5 units of the network token
   }),
   ctcBob.p.Bob({
     // implement Bob's interact object here
-    ...Player('Bob')
+    ...Player('Bob'),
+    acceptWager: (amt) => {
+      console.log(`Bob accepts the wager of ${fmt(amt)}.`);//show wageer and immedetely accept it by returning it
+    },
   }),
 ]);
+//get balance afterwards
+const afterAlice = await getBalance(accAlice);
+const afterBob = await getBalance(accBob);
+
+//print balance afterwards(effect)
+console.log(`Alice went from ${beforeAlice } to ${ afterAlice}.`)
+console.log(`Bob went from ${beforeBob} to ${afterBob}.`);
